24fc54719a4ab72f3100228264b2343a174097ae
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-04 21:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const button=document.getElementById("btn");
+const msginp=document.getElementById("text");
+const delayinp=document.getElementById("delay");
+const output=document.getElementById("output");
+
+button.addEventListener("click",()=>{
+	const msg=msginp.value;
+	const delay=Number(delayinp.value);
+	if(msg && delay>0){
+		call(msg,delay);
+	}
+})
+async function call(msg,delay){
+	await new Promise((resolve)=>{
+			setTimeout((dealy)=>{
+				resolve();
+			},delay)
+		})
+	output.innerText=msg;
+}
